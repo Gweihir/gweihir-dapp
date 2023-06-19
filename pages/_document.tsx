d8b3de9857458e4b611437dd95e3c07968dc2e08
Astral Gnome <reedkeenan@gmail.com>
2023-06-19 01:35:49 -0700

    Added some metadata to head in document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,30 @@ export default function Document() {
       <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
       <link rel='manifest' href='/favicon/site.webmanifest' />
 
+      <meta property='og:url' content='https://gweihir.app' />
+      <meta property='og:type' content='website' />
+      <meta
+        property='og:title'
+        content='Discover Project Gweihir: Bridging Kusama to Ethereum via Chainlink'
+      />
+      <meta property='og:image' content='https://gweihir.app/Images/Project-Gwei-Logo.png' />
+      <meta
+        property='og:description'
+        content={`An open-source project to service Kusama's chain data to the Ethereum network via a Chainlink external adapter.`}
+      />
+      <meta property='og:image:alt' content='Project Gweihir thumbnail image' />
+      {/* TWITTER */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta
+        name='twitter:title'
+        content='Discover Project Gweihir: Bridging Kusama to Ethereum via Chainlink'
+      />
+      <meta
+        name='twitter:description'
+        content={`An open-source project to service Kusama's chain data to the Ethereum network via a Chainlink external adapter.`}
+      />
+      <meta name='twitter:image' content='https://gweihir.app/Images/Project-Gwei-Logo.png' />
+
       <body>
         <Main />
         <NextScript />
